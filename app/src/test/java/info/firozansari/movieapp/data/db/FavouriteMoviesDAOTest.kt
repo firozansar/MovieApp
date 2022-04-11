2e0103497cb2bc6f3dc4a888d560118665f2bcb3
Firoz Ansari <firozansar@gmail.com>
2022-04-11 14:55:50 +0100

    Refactoring

---
+++ app/src/test/java/info/firozansari/movieapp/data/db/FavouriteMoviesDAOTest.kt
@@ -0,0 +1,33 @@
+package info.firozansari.movieapp.data.db
+
+import info.firozansari.movieapp.MockTestUtil.Companion.mockMovieList
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert
+import org.junit.Test
+
+class FavouriteMoviesDAOTest: LocalDatabase() {
+    @Test
+    fun insertAndReadTest() {
+        val mockData = mockMovieList()
+//        db.favoriteMoviesDAO().saveFavorite(mockData[0])
+//        val loadFromDB = db.movieDao().getMovieList(1)[0]
+//        MatcherAssert.assertThat(loadFromDB.page, CoreMatchers.`is`(1))
+//        MatcherAssert.assertThat(loadFromDB.id, CoreMatchers.`is`(123))
+    }
+
+    @Test
+    fun updateAndReadTest() {
+        val mockData = mockMovieList()
+//        val movie = mockMovie()
+//        db.movieDao().insertMovieList(mockData)
+//
+//        val loadFromDB = db.movieDao().getMovie(movie.id)
+//        MatcherAssert.assertThat(loadFromDB.page, CoreMatchers.`is`(1))
+//
+//        movie.page = 10
+//        db.movieDao().updateMovie(movie)
+//
+//        val updated = db.movieDao().getMovie(movie.id)
+//        MatcherAssert.assertThat(updated.page, CoreMatchers.`is`(10))
+    }
+}
---
+++ app/src/test/java/info/firozansari/movieapp/data/db/LocalDatabase.kt
@@ -0,0 +1,25 @@
+package info.firozansari.movieapp.data.db
+
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import org.junit.After
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+abstract class LocalDatabase {
+  lateinit var db: AppDatabase
+
+  @Before
+  fun initDB() {
+    db = Room.inMemoryDatabaseBuilder(getApplicationContext(), AppDatabase::class.java)
+      .allowMainThreadQueries()
+      .build()
+  }
+
+  @After
+  fun closeDB() {
+    db.close()
+  }
+}
