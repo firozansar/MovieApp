e8da7b91bb720ee8cd543b651cd4d349958ad5c7
Firoz Ansari <firozansar@gmail.com>
2022-07-31 09:46:06 +0100

    Refactoring

--- app/src/main/java/info/firozansari/movieapp/presentation/components/BubbleLoader.kt
+++ app/src/main/java/info/firozansari/movieapp/presentation/components/BubbleLoader.kt
@@ -1,4 +1,4 @@
-package info.firozansari.movieapp.presentation.components.loader
+package info.firozansari.movieapp.presentation.components
 
 import android.animation.Animator
 import android.animation.AnimatorSet
